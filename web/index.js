697acbfa7f2eb34d72f553e4ecc0a98f7168cb56
Jie Chen <jiechenmc@gmail.com>
2020-07-16 12:05:25 -0400

    minor change

--- web/index.js
+++ web/index.js
@@ -1,9 +1,16 @@
 const request = new XMLHttpRequest();
+const paragraph = document.querySelector("p");
 
 request.addEventListener("readystatechange", () => {
-    if (request.readyState === 4) {
-        console.log(request.responseText)
-    }
-})
+  if (request.readyState === 4) {
+    console.log(request.responseText);
+    paragraph.innerText = request.responseText;
+  }
+});
 
-request.send("https://api.hypixel.net/skyblock/bazaar?key=191e62a6-2d3e-49b1-b58e-106f0e16bb1f")
+request.open(
+  "GET",
+  "https://api.hypixel.net/skyblock/bazaar?key=191e62a6-2d3e-49b1-b58e-106f0e16bb1f"
+);
+
+request.send();
