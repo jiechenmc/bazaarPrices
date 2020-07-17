b76db544eda111177b2e3556689ba18aa5f22720
Jie Chen <jiechenmc@gmail.com>
2020-07-16 20:39:04 -0400

    callbacks

--- web/index.js
+++ web/index.js
@@ -1,16 +1,26 @@
 const request = new XMLHttpRequest();
 const paragraph = document.querySelector("p");
 
-request.addEventListener("readystatechange", () => {
-  if (request.readyState === 4) {
-    console.log(request.responseText);
-    paragraph.innerText = request.responseText;
+function getRequest(cb) {
+  request.addEventListener("readystatechange", () => {
+    if ((request.readyState === 4) & (request.status === 200)) {
+      cb(undefined, request.responseText);
+    } else {
+      console.log("ERROR!", undefined);
+    }
+  });
+
+  request.open(
+    "GET",
+    "https://api.hypixel.net/skyblock/bazaar?key=191e62a6-2d3e-49b1-b58e-106f0e16bb1f"
+  );
+
+  request.send();
+}
+
+getRequest((err, data) => {
+  if (!err) {
+    console.log(data);
+    paragraph.innerText = data;
   }
 });
-
-request.open(
-  "GET",
-  "https://api.hypixel.net/skyblock/bazaar?key=191e62a6-2d3e-49b1-b58e-106f0e16bb1f"
-);
-
-request.send();
