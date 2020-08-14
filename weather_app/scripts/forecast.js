2c277467768fb8dcd804a090f061697e8bdc0b28
Jie Chen <jiechenmc@gmail.com>
2020-08-13 20:16:58 -0400

    got data

---
+++ weather_app/scripts/forecast.js
@@ -0,0 +1,17 @@
+const getItem = async (item) => {
+  const url = "https://api.hypixel.net/skyblock/bazaar";
+  const key = "191e62a6-2d3e-49b1-b58e-106f0e16bb1f";
+  const query = `${url}?key=${key}`;
+  const resposne = await fetch(query);
+  const { products } = await resposne.json();
+  const quickStatus = products[item].quick_status;
+  return quickStatus;
+};
+
+const ITEM = "ENCHANTED_OAK_LOG";
+getItem(ITEM).then((data) => {
+  itemName = data.productId;
+  buyPrice = data.buyPrice;
+  sellPrice = data.sellPrice;
+  console.log(itemName, buyPrice, sellPrice);
+});
