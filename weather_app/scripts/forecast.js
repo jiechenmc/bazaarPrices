5bcdf09b06c3237e4710286f3b4efc2112145bb1
Jie Chen <jiechenmc@gmail.com>
2020-08-13 20:45:41 -0400

    error handling

--- weather_app/scripts/forecast.js
+++ weather_app/scripts/forecast.js
@@ -9,9 +9,13 @@ const getItem = async (item) => {
 };
 
 const ITEM = "ENCHANTED_OAK_LOG";
-getItem(ITEM).then((data) => {
-  itemName = data.productId;
-  buyPrice = data.buyPrice;
-  sellPrice = data.sellPrice;
-  console.log(itemName, buyPrice, sellPrice);
-});
+getItem(ITEM)
+  .then((data) => {
+    itemName = data.productId;
+    buyPrice = data.buyPrice;
+    sellPrice = data.sellPrice;
+    console.log(itemName, buyPrice, sellPrice);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
