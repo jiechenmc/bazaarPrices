1ced7f43ae68fbaeade2b9827449a656729a00c7
Jie Chen <jiechenmc@gmail.com>
2020-08-14 21:11:27 -0400

    products

--- pythonScripts/getproducts.py
+++ pythonScripts/getproducts.py
@@ -1,3 +1,11 @@
 import requests
 
-r = requests.get("")
+r = requests.get(
+    "https://api.hypixel.net/skyblock/bazaar?key=191e62a6-2d3e-49b1-b58e-106f0e16bb1f"
+).json()
+
+products = [product + "\n" for product in r["products"]]
+products[-1] = products[-1].replace("\n", "")
+
+with open("../products.txt", "w") as f:
+    f.writelines(products)
